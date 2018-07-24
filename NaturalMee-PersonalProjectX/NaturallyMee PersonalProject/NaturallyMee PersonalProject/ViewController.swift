fb0f34ff55a4f42a729fa130a139bcc983439e59
Marianna <mckd2018@gmail.com>
2018-07-24 15:26:27 -0700

    first commit with xcode project

---
+++ NaturalMee-PersonalProjectX/NaturallyMee PersonalProject/NaturallyMee PersonalProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NaturallyMee PersonalProject
+//
+//  Created by Mari Campbell on 7/24/18.
+//  Copyright © 2018 Marianna Campbell. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
