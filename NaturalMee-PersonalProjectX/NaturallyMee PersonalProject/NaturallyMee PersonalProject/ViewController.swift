05957f6e11fe0a365132a4f16367e746f5fe6680
Marianna <mckd2018@gmail.com>
2018-07-25 17:55:41 -0700

    MVP Storyboards

--- NaturalMee-PersonalProjectX/NaturallyMee PersonalProject/NaturallyMee PersonalProject/ViewController.swift
+++ NaturalMee-PersonalProjectX/NaturallyMee PersonalProject/NaturallyMee PersonalProject/ViewController.swift
@@ -9,11 +9,29 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    // MARK: IBOutlets
+    @IBOutlet weak var signupButton: UIButton!
+    @IBOutlet weak var loginButton: UIButton!
+   
+//    override func viewWillAppear(_ animated: Bool) {
+//        super.viewWillAppear(animated)
+//
+//        navigationController?.setNavigationBarHidden(true, animated: false)
+//    }
+//    override func viewDidAppear(_ animated: Bool) {
+//        super.viewDidAppear(animated)
+//    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
+    
+//    override func viewDidDisappear(_ animated: Bool) {
+//        super.viewDidDisappear(animated)
+//
+//        navigationController?.setNavigationBarHidden(false, animated: true)
+//    }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
